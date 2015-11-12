0b7097b8178e65d9ce15ea18394108cfd0c9e5ae
nfoote <nfoote@live.co.uk>
2015-11-12 21:20:30 +0000

    Evolved changes all over the joint

--- src/main/java/pptick/web/HomeController.java
+++ src/main/java/pptick/web/HomeController.java
@@ -1,47 +1,24 @@
 package pptick.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
-import com.sendgrid.SendGrid;
-import com.sendgrid.SendGridException;
-import com.sendgrid.SendGrid.Email;
-
-import pptick.domain.TickList;
-import pptick.domain.mongo.TickListMongoRepo;
-import pptick.domain.mongo.TickMongoRepo;
 import pptick.scheduled.PPChecker;
-import pptick.service.PPCheckerService;
+
+import com.sendgrid.SendGrid;
+import com.sendgrid.SendGrid.Email;
+import com.sendgrid.SendGridException;
  
 @Controller
 class HomeController {
 	
-	@Value("#{'${pp.properties}'.split(',')}") 
-	private List<String> properties;
-	
-	@Autowired
-	private TickMongoRepo tickMongoRepo;
-	
-	@Autowired
-	private TickListMongoRepo tickListMongoRepo;
-	
 	@Autowired
 	private PPChecker ppChecker;
-	
-	@Autowired
-	private PPCheckerService ppCheckerService;
 	
 	@Autowired
 	private SendGrid sendGrid;
@@ -56,7 +33,6 @@ class HomeController {
     public ModelAndView index() {
     	ModelAndView mav = new ModelAndView("index");
     	mav.addObject("started", ppChecker.getRun());
-    	mav.addObject("properties", properties);
         return mav;
     }
  
@@ -69,12 +45,6 @@ class HomeController {
     @RequestMapping("/stop")
     public View stop() {
     	ppChecker.setRun(false);
-        return new RedirectView("/");
-    }
- 
-    @RequestMapping("/process-ticks")
-    public View processTicks() {
-    	ppCheckerService.processTicksToTickLists();
         return new RedirectView("/");
     }
  
@@ -95,16 +65,5 @@ class HomeController {
 		}
 		
         return new RedirectView("/");
-    }
-
-    @RequestMapping("/property/{propertyId}")
-    public ModelAndView view(@PathVariable String propertyId, @RequestParam(required=false) Integer page) {
-    	ModelAndView mav = new ModelAndView("property");
-    	mav.addObject("propertyId", propertyId);
-    	
-    	Page<TickList> tickLists = tickListMongoRepo.findByPropertyId(propertyId, new PageRequest(page == null ? 0 : page, 100, Direction.DESC, "date"));
-    	mav.addObject("tickLists", tickLists);
-    	
-    	return mav;
     }
 }
